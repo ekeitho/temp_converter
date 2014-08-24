75eb613ad03ebfffb504565dae49a2b30d2de742
Keith Abdulla <email@ekeitho.com>
2014-08-24 00:20:31 -0700

    started writing test and using travis

---
+++ convert.js
@@ -0,0 +1,6 @@
+function converter(format_to_convert, degree) {   
+   if (formater_to_convert=="c")
+     return 9/5*(degree+32);
+   else
+     return 5/9*(degree-32);
+}
---
+++ tests/celsius.js
@@ -0,0 +1,12 @@
+describe("celsius tests", function ()
+{
+   var convert = require("../converter").converter;
+   var expect = require("chai").expect;
+
+   it("true tests", function() {
+      expect(convert("c", 32).to.equal(93.6);      
+
+   });
+
+
+}
