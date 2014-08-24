12a981f105f7b9e85a04ca44c91b1e2b0b63c012
Keith Abdulla <email@ekeitho.com>
2014-08-24 00:25:08 -0700

    started writing test and using travis

--- test/celsius.js
+++ test/celsius.js
@@ -4,9 +4,6 @@ describe("celsius tests", function ()
    var expect = require("chai").expect;
 
    it("true tests", function() {
-      expect(convert("c", 32).to.equal(93.6);      
-
+      expect(convert("c", 32).to.equal(93.6)); 
    });
-
-
-}
+});
